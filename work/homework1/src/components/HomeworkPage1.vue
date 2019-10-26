2ab8d28176e2382a68b3d69be2ce9d0b20f13e44
richarmos <richarmos@163.com>
2019-10-26 12:52:46 +0800

    第一次修改尝试

---
+++ work/homework1/src/components/HomeworkPage1.vue
@@ -0,0 +1,3 @@
+<template>
+<h1> This is HomeworkPage1 </h1>
+</template>
--- work/homework1/src/router/index.js
+++ work/homework1/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import HomeworkPage1 from '@/components/HomeworkPage1'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/HomeworkPage1',
+      name: 'HomeworkPage1',
+      component: HomeworkPage1
     }
   ]
 })
